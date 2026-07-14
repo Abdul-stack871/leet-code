f59a0baba2ab48649e6cbc0a0e0596541dd9e31b
Abdul Rahman <abdulrahumanrock15@gmail.com>
2026-07-14 14:25:00 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,10 @@
+// Last updated: 7/14/2026, 2:25:00 PM
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n % 4 != 0){
+            return true ;
+        }else {
+            return false ;
+        }
+    }
+}
