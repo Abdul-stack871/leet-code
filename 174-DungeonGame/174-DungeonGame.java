d05158fd4df428330b43af0262a111421c7ff42e
Abdul Rahman <abdulrahumanrock15@gmail.com>
2026-07-14 14:25:32 +0530

    Create 174-DungeonGame

---
+++ 174-DungeonGame/174-DungeonGame.java
@@ -0,0 +1,30 @@
+// Last updated: 7/14/2026, 2:25:31 PM
+class Solution {
+    public int calculateMinimumHP(int[][] dungeon) {
+        if (dungeon == null || dungeon.length == 0 || dungeon[0].length == 0) return 0;
+        
+        int m = dungeon.length;
+        int n = dungeon[0].length;
+        
+        int[][] dp = new int[m][n];
+        
+        dp[m - 1][n - 1] = Math.max(1, 1 - dungeon[m - 1][n - 1]);
+        
+        for (int i = m - 2; i >= 0; i--) {
+            dp[i][n - 1] = Math.max(1, dp[i + 1][n - 1] - dungeon[i][n - 1]);
+        }
+        
+        for (int j = n - 2; j >= 0; j--) {
+            dp[m - 1][j] = Math.max(1, dp[m - 1][j + 1] - dungeon[m - 1][j]);
+        }
+        
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                int minHealthNext = Math.min(dp[i + 1][j], dp[i][j + 1]);
+                dp[i][j] = Math.max(1, minHealthNext - dungeon[i][j]);
+            }
+        }
+        
+        return dp[0][0];
+    }
+}
