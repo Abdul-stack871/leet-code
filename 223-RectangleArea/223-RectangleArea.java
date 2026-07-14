fb512fd54e476b3a541483fdccc5364487e075d9
Abdul Rahman <abdulrahumanrock15@gmail.com>
2026-07-14 14:25:16 +0530

    Create 223-RectangleArea

---
+++ 223-RectangleArea/223-RectangleArea.java
@@ -0,0 +1,19 @@
+// Last updated: 7/14/2026, 2:25:16 PM
+class Solution {
+    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        long areaA = (long)(ax2 - ax1) * (ay2 - ay1);
+        long areaB = (long)(bx2 - bx1) * (by2 - by1);
+
+        int left = Math.max(ax1, bx1);
+        int right = Math.min(ax2, bx2);
+        int bottom = Math.max(ay1, by1);
+        int top = Math.min(ay2, by2);
+
+        long overlap = 0;
+        if (right > left && top > bottom) {
+            overlap = (long)(right - left) * (top - bottom);
+        }
+
+        return (int)(areaA + areaB - overlap);
+    }
+}
