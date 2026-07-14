593d68588151501f40d00d33755609860193812e
Abdul Rahman <abdulrahumanrock15@gmail.com>
2026-07-14 14:25:26 +0530

    Create 188-BestTimeToBuyAndSellStockIv

---
+++ 188-BestTimeToBuyAndSellStockIv/188-BestTimeToBuyAndSellStockIv.java
@@ -0,0 +1,26 @@
+// Last updated: 7/14/2026, 2:25:26 PM
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        if (n <= 1) return 0;
+        
+        // If k is large enough, this becomes the classic "unlimited transactions" problem
+        if (k >= n / 2) {
+            int maxPro = 0;
+            for (int i = 1; i < n; i++) {
+                if (prices[i] > prices[i - 1]) maxPro += prices[i] - prices[i - 1];
+            }
+            return maxPro;
+        }
+        
+        int[][] dp = new int[k + 1][n];
+        for (int i = 1; i <= k; i++) {
+            int localMax = dp[i - 1][0] - prices[0];
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.max(dp[i][j - 1], prices[j] + localMax);
+                localMax = Math.max(localMax, dp[i - 1][j] - prices[j]);
+            }
+        }
+        return dp[k][n - 1];
+    }
+}
