fb3162fe7fbb8b1dd3918f1c893a3a43b85993c3
Abdul Rahman <abdulrahumanrock15@gmail.com>
2026-08-11 11:39:42 +0530

    File updated at 8/11/2026, 11:39:43 AM

--- 988-SmallestStringStartingFromLeaf/988-SmallestStringStartingFromLeaf.java
+++ 988-SmallestStringStartingFromLeaf/988-SmallestStringStartingFromLeaf.java
@@ -1,32 +1,18 @@
-// Last updated: 8/11/2026, 11:30:53 AM
+// Last updated: 8/11/2026, 11:39:43 AM
 1class Solution {
-2    String ans = "";
-3
-4    public String smallestFromLeaf(TreeNode root) {
-5        find(root, "");
-6        return ans;
-7    }
-8
-9    void find(TreeNode root, String s) {
+2
+3    public String tree2str(TreeNode root) {
+4
+5        if (root == null)
+6            return "";
+7
+8        if (root.left == null && root.right == null)
+9            return root.val + "";
 10
-11        if (root == null)
-12            return;
+11        if (root.right == null)
+12            return root.val + "(" + tree2str(root.left) + ")";
 13
-14        char ch = (char)('a' + root.val);
-15
-16        // Add character in front
-17        s = ch + s;
-18
-19        // Leaf node
-20        if (root.left == null && root.right == null) {
-21
-22            if (ans.equals("") || s.compareTo(ans) < 0)
-23                ans = s;
-24
-25            return;
-26        }
-27
-28        find(root.left, s);
-29        find(root.right, s);
-30    }
-31}
+14        return root.val + "(" + tree2str(root.left) + ")" 
+15                     + "(" + tree2str(root.right) + ")";
+16    }
+17}
