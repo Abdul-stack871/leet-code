9b7ecd32bc1471372f5256b6f1de55b26728d224
Abdul Rahman <abdulrahumanrock15@gmail.com>
2026-09-11 10:12:55 +0530

    Create 3483-Unique3-DigitEvenNumbers

---
+++ 3483-Unique3-DigitEvenNumbers/3483-Unique3-DigitEvenNumbers.java
@@ -0,0 +1,32 @@
+// Last updated: 9/11/2026, 10:12:54 AM
+1class Solution {
+2    public int totalNumbers(int[] digits) {
+3        int[] digitFreq = new int[10];
+4        for (int digit : digits) {
+5            digitFreq[digit]++;
+6        }
+7        
+8        int ans = 0;
+9        for (int i = 100; i < 999; i += 2) {
+10            int num = i;
+11            HashMap<Integer, Integer> map = new HashMap<>();
+12            
+13            while (num > 0) {
+14                int rem = num % 10;
+15                map.put(rem, map.getOrDefault(rem, 0) + 1);
+16                num = num / 10;
+17            }
+18            
+19            boolean flag = true;
+20            for (int d : map.keySet()) {
+21                if (digitFreq[d] < map.get(d)) {
+22                    flag = false;
+23                    break;
+24                }
+25            }
+26            
+27            if (flag) ans++;
+28        }
+29        return ans;
+30    }
+31}
